1bcbaf0781c8e6f03fb9bf0bc7bcdaa62f20bd21
YoussefShabo <youssefshabo@gmail.com>
2023-07-17 17:02:33 +0200

    updated the logo

--- movie-library/app/page.jsx
+++ movie-library/app/page.jsx
@@ -1,12 +1,15 @@
 import Image from "next/image";
 import Movie from "./Movie"
-
+import Head from 'next/head'
 
 export default async function Home() {
   const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`)
   const res = await data.json()
   console.log(res)
   return <main>
+          <Head>
+        <meta property="og:title" content="YS MovieDB" key="title" />
+      </Head>
     <h1>YS MovieDB 🍿</h1>
     <div className="grid gap-16 grid-cols-fluid">
     {res.results.map((movie) => (
